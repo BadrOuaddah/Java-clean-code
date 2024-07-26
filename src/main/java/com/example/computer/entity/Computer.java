df62f90b6f3252ee430c25182b2abb2675dad229
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-07-26 12:04:05 +0100

    Fix errors of checkstyle in entity layer

--- src/main/java/com/example/computer/entity/Computer.java
+++ src/main/java/com/example/computer/entity/Computer.java
@@ -1,6 +1,3 @@
-/**
- * This package is about Computer entity with attributes
- */
 package com.example.computer.entity;
 
 import jakarta.persistence.Entity;
@@ -12,21 +9,56 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+/**
+ * Represents a computer entity with attributes such as brand, model, and specifications.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
 public class Computer {
+
+    /**
+     * The unique identifier for the computer.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    /**
+     * The brand of the computer.
+     */
     private String brand;
+
+    /**
+     * The model of the computer.
+     */
     private String model;
+
+    /**
+     * The processor details of the computer.
+     */
     private String processor;
+
+    /**
+     * The amount of RAM in the computer.
+     */
     private String ram;
+
+    /**
+     * The storage capacity of the computer.
+     */
     private String storage;
+
+    /**
+     * The operating system installed on the computer.
+     */
     private String operatingSystem;
+
+    /**
+     * The price of the computer.
+     */
     private double price;
 
 }
---
+++ src/main/java/com/example/computer/entity/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the entity classes for the Computer application.
+ */
+package com.example.computer.entity;
