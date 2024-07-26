ad63afd1818b0e35765944c4eeaeb625d6631299
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-07-26 12:04:45 +0100

    Fix errors of checkstyle in repository layer

---
+++ src/main/java/com/example/computer/repository/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains repository interfaces for the Computer application.
+ * The repositories provide CRUD operations and custom queries for the
+ * application entities.
+ */
+package com.example.computer.repository;
