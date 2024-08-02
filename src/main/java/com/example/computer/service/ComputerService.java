56c95a3f7643279337cf4d71ce2b3a7955842685
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-08-02 17:07:14 +0100

    Fix errors of checkstyle in service

--- src/main/java/com/example/computer/service/ComputerService.java
+++ src/main/java/com/example/computer/service/ComputerService.java
@@ -7,36 +7,75 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing Computers.
+ */
 @Service
-public class ComputerService {
+public final class ComputerService {
 
     private final ComputerRepository computerRepository;
 
-    public ComputerService(ComputerRepository computerRepository) {
+    /**
+     * Constructor for ComputerService.
+     *
+     * @param computerRepository the computer repository
+     */
+    public ComputerService(final ComputerRepository computerRepository) {
         this.computerRepository = computerRepository;
     }
 
-    public Optional<Computer> getOneComputer(Long id) {
+    /**
+     * Get one computer by ID.
+     *
+     * @param id the ID of the computer
+     * @return the computer if found
+     */
+    public Optional<Computer> getOneComputer(final Long id) {
         return computerRepository.findById(id);
     }
 
+    /**
+     * Get all computers.
+     *
+     * @return the list of all computers
+     */
     public List<Computer> getAllComputer() {
         return computerRepository.findAll();
     }
 
-    public Computer addComputer(Computer computer) {
+    /**
+     * Add a new computer.
+     *
+     * @param computer the computer to add
+     * @return the added computer
+     */
+    public Computer addComputer(final Computer computer) {
         return computerRepository.save(computer);
     }
 
-    public Computer updateComputer(Long id, Computer computer) throws Exception {
-        computerRepository.findById(id).orElseThrow(() -> new Exception("Document not found with id : " + id));
+    /**
+     * Update an existing computer.
+     *
+     * @param id the ID of the computer to update
+     * @param computer the updated computer details
+     * @return the updated computer
+     * @throws Exception if the computer is not found
+     */
+    public Computer updateComputer(final Long id, final Computer computer) throws Exception {
+        computerRepository.findById(id)
+                .orElseThrow(() -> new Exception("Document not found with id : " + id));
         return computerRepository.save(computer);
     }
 
-    public void deleteComputer(Long id) throws Exception {
-        computerRepository.findById(id).orElseThrow(() -> new Exception("Document not found with id : " + id));
+    /**
+     * Delete a computer by ID.
+     *
+     * @param id the ID of the computer to delete
+     * @throws Exception if the computer is not found
+     */
+    public void deleteComputer(final Long id) throws Exception {
+        computerRepository.findById(id)
+                .orElseThrow(() -> new Exception("Document not found with id : " + id));
         computerRepository.deleteById(id);
     }
-
-
 }
